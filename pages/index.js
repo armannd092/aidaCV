6940988be2fcc3ee2e5bc40f22e49c753722a85e
armannd092 <>
2022-01-01 22:20:04 +0100

    add image component from next

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import About from '../src/component/About'
 import Skill from '../src/component/Skills'
+import Image from 'next/image'
 
 
 export default function Home() {
-  const profile =<img className="profile" width="500px" height="500px" src={require('./../public/Aida.jpg')} />
+  //const profile =<img className="profile" width="500px" height="500px"  src={require('./../public/Aida.jpg')}/>
+  const profile =<Image className="profile" width="500px" height="500px" src='/Aida.jpg'/>
+  
   return(
     <>
       <About image = {profile}/>
