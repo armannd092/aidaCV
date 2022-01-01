4baa2c42b5985a121cc360d85dcd6a3e5bf8d6b4
armannd092 <>
2022-01-01 21:41:19 +0100

    fix relative path for img

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Skill from '../src/component/Skills'
 
 
 export default function Home() {
-  const profile =<img className="profile" width="500px" height="500px" src={require('/../public/Aida.jpg')} />
+  const profile =<img className="profile" width="500px" height="500px" src={require('./../public/Aida.jpg')} />
   return(
     <>
       <About image = {profile}/>
